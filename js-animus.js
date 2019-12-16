c4efba56924846e29db463c99366b9341d9c5662
Did <did.rossi@gmail.com>
2019-12-16 22:17:57 +0100

    Not hardcoded reading of value from event (with static configuration).

--- js-animus.js
+++ js-animus.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 // Dependencies
+var HashMap = require('hashmap');
 require('dotenv').config()
 const Influx = require('influx');
 const WebSocket = require('ws');
@@ -16,8 +17,8 @@ const logger = winston.createLogger({
     // - Write to all logs with level `debug` and below to `/tmp/log-heart2info.log'
     // - Write all logs error (and below) to `error.log`.
     //
-    new winston.transports.File({ filename: '/tmp/error.log', level: 'error' }),
-    new winston.transports.File({ filename: '/tmp/log-heart2info.log' })
+    // DEBUG new winston.transports.File({ filename: '/tmp/error.log', level: 'error' }),
+    new winston.transports.File({ filename: '/tmp/log-heart.log' })
   ]
 });
 
@@ -39,6 +40,21 @@ process.on('uncaughtException', function(error) {
    process.exit(1)
 });
 
+var devices= new HashMap();
+var functions= new HashMap();
+var locations= new HashMap();
+
+locations.set("area-0-1575580173183", "Couloir");
+locations.set("area-8-1575149704169", "Bureau");
+
+devices.set("com.animushome.heart.packages.rf433.1d16ea82-9ce4-4e30-af75-80415e0c30d3",{"name":"Tellus", "animus_area": "area-8-1575149704169"});
+devices.set("com.animushome.heart.packages.zwave.devices.010F-6",{"name":"Detecteur fumée","animus_area":"area-0-1575580173183"});
+devices.set("com.animushome.heart.packages.zwave.devices.0060-8",{"name":"Motion couloir","animus_area":"area-0-1575580173183"});
+
+functions.set("com.animushome.heart.packages.rf433.1d16ea82-9ce4-4e30-af75-80415e0c30d3:f-8193.0", {"device_UID": "com.animushome.heart.packages.rf433.1d16ea82-9ce4-4e30-af75-80415e0c30d3", "type": "temperature", "clazz": "com.animushome.heart.service.dal.functions.MultiLevelSensor"});
+functions.set("com.animushome.heart.packages.rf433.1d16ea82-9ce4-4e30-af75-80415e0c30d3:f-8193.1", {"device_UID": "com.animushome.heart.packages.rf433.1d16ea82-9ce4-4e30-af75-80415e0c30d3", "type": "humidity", "clazz": "com.animushome.heart.service.dal.functions.MultiLevelSensor"});
+functions.set("com.animushome.heart.packages.zwave.devices.010F-6:f-0.49.1", {"device_UID": "com.animushome.heart.packages.zwave.devices.010F-6:f-0.49.1", "type": "temperature", "clazz": "com.animushome.heart.service.dal.functions.MultiLevelSensor"});
+functions.set("com.animushome.heart.packages.zwave.devices.0060-8:f-0.48", {"device_UID": "com.animushome.heart.packages.zwave.devices.0060-8", "type": null, "clazz": "com.animushome.heart.service.dal.functions.BooleanSensor"});
 
 // Init Animushome heart stuff
 const wsUri = "ws://" + process.env.HEART_IP + "/heart/events";
@@ -56,7 +72,8 @@ const influx = new Influx.InfluxDB({
      fields: {
        temperature: Influx.FieldType.FLOAT,
        humidity: Influx.FieldType.INTEGER,
-       presence: Influx.FieldType.BOOLEAN
+       presence: Influx.FieldType.BOOLEAN,
+       unit: Influx.FieldType.STRING
      },
      tags: [
        'name', 'location'
@@ -66,23 +83,21 @@ const influx = new Influx.InfluxDB({
 })
 
 // Create the sensor object
-var sensor = {name:"", location:null, temperature:null, humidity:null, presence:null};
+var sensor = {name:"", location:null, temperature:null, humidity:null, presence:null, unit:null};
 
 
-function myPing() {
+function pingHeart() {
   logger.debug("Send heartbeat");
   websocket.send("heartbeat");
-  setTimeout(myPing, 50000);
+  setTimeout(pingHeart, 50000);
 }
 
 websocket.onopen = function(evt) {
-  // THIS IS NOT working logger.info("ws open", evt);
-  logger.info("ws open");
-  logger.info(JSON.stringify(evt,null,2));
+  logger.info("ws open:" + JSON.stringify(evt,null,2));
   //First message after connection open must be the Authorization message
   //Send Authorization message within 2 seconds, otherwise socket will get closed
   websocket.send("Authorization: Bearer " + process.env.HEART_API_KEY);
-  setTimeout(myPing, 50000);
+  setTimeout(pingHeart, 50000);
 };
 
 websocket.onclose = function(evt) {
@@ -109,56 +124,67 @@ websocket.onmessage = function(evt) {
         var ts = new Date(resp.value.timestamp);
         logger.info("timestamp:    " + ts.toGMTString());
 	
-	var value  = resp.value.level;
 	sensor.temperature = null;
 	sensor.humidity    = null;
 	sensor.presence    = null;
+	sensor.unit        = resp.value.unit;
 
-	// Fibaro smoke detector
-	if (resp.functionUID.match(/f-0\.49\.1$/) ){ 
-	   sensor.name = "Smoke detector";
-	   sensor.location = "Couloir";
-	   sensor.temperature = value;
-	}
+	var func, dev;
+	if (functions.has(resp.functionUID)) {
 
-	// Tellus thermo & hygro
-	if (resp.functionUID.match(/f-8193\.0$/) ) { 
-	   sensor.name = "Tellus"
-	   sensor.location = "Bureau"
-	   sensor.temperature = value;
-	}
-
-	if (resp.functionUID.match(/f-8193\.1$/) ) { 
-	   sensor.name = "Tellus"
-	   sensor.location = "Bureau"
-	   sensor.humidity = value;
-	}
-
-    	// Everspring presence sensor
-	if (resp.functionUID.match(/f-0\.48$/) )   { 
-	   sensor.name = "Everspring"
-	   sensor.location = "Couloir"
-	   sensor.presence = resp.value.value;
-	   if (resp.value.value) {
-	      value=1;
+ 	   func = functions.get(resp.functionUID); 
+    	   if (func.clazz === "com.animushome.heart.service.dal.functions.MultiLevelSensor") {
+              if (func.type === "temperature") {
+	         sensor.temperature = resp.value.level;
+	      } 
+	      else if (func.type === "humidity") {
+	         sensor.humidity = resp.value.level;
+	      }
+	      else {
+	         logger.warn("Humm... unsupported type (" + func.type + "). We should not be here");
+	      }
+           }
+	   else {
+	      // loc.class === "com.animushome.heart.service.dal.functions.BooleanSensor"
+	      sensor.presence = resp.value.value;
+	   }
+	   if (devices.has(func.device_UID)) {
+	      dev = devices.get(func.device_UID);
+	      sensor.name = dev.name;
+	      sensor.location = locations.get(dev.animus_area);
 	   }
 	   else {
-	      value=0;
+	      logger.warn("Unsupported device (do a restart to force reading of devices)");
 	   }
-        }
+	} 
+	else {
+	   logger.warn("Unknown function IUD : " + resp.functionUID);
+	}
+
 
         logger.debug("sensor object:");
         logger.debug(JSON.stringify(sensor,null,2));
 	// Write value to InfluxDB
         if ( (sensor.temperature != null) || (sensor.humidity != null) || (sensor.presence != null) ) {
            logger.info("Write to influxDB");
-	   influx.writePoints([
+           logger.debug("DO NOT Write to influxDB");
+	   if (1) {
+	      logger.debug("Write this to influxDB");
+	      logger.debug(JSON.stringify({
+    	            measurement: 'sensor',
+    	            tags: { name: sensor.name, location: sensor.location },
+    	               fields: { temperature: sensor.temperature, humidity: sensor.humidity, presence: sensor.presence, unit: sensor.unit }
+  	         }),null,2);
+	   }
+	   else {
+	      influx.writePoints([
   	      {
     	         measurement: 'sensor',
     	         tags: { name: sensor.name, location: sensor.location },
-    	         fields: { temperature: sensor.temperature, humidity: sensor.humidity, presence: sensor.presence }
+    	         fields: { temperature: sensor.temperature, humidity: sensor.humidity, presence: sensor.presence, unit: sensor.unit }
   	      }
-	   ]);
+	      ]);
+	   }
 	}
   }
   catch(err) {
